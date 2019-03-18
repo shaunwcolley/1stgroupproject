5de6e210e7d06cc318f528305663b2a9dd1a2805
Shaun W Colley <swc7838@gmail.com>
2019-03-18 14:34:44 -0500

    everything is function, needstyling

--- registration.js
+++ registration.js
@@ -2,6 +2,12 @@ let firstNameTextBox = document.getElementById("firstNameTextBox")
 let lastNameTextBox = document.getElementById("lastNameTextBox")
 let emailTextBox = document.getElementById("emailTextBox")
 let passwordTextBox = document.getElementById("passwordTextBox")
+let cityTextBox = document.getElementById("cityTextBox")
+let stateTextBox = document.getElementById("stateTextBox")
+let weightTextBox = document.getElementById("weightTextBox")
+let feetTextBox = document.getElementById("feetTextBox")
+let inchesTextBox = document.getElementById("inchesTextBox")
+
 let registerBtn = document.getElementById("registerBtn")
 let database = firebase.database()
 let usersRef = database.ref('users')
@@ -13,16 +19,28 @@ registerBtn.addEventListener('click', function(e) {
   let password = passwordTextBox.value
   let firstName = firstNameTextBox.value
   let lastName = lastNameTextBox.value
+  let city = cityTextBox.value
+  let state = stateTextBox.value
+  let weight = weightTextBox.value
+  let feet = feetTextBox.value
+  let inches = inchesTextBox.value
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(function(response){
     let userRef = usersRef.child(response.user.uid)
     userRef.set({
       firstName: firstName,
-      lastName: lastName
+      lastName: lastName,
+      location: {
+        city: city,
+        state: state,
+      },
+      weight: weight,
+      height: {feet: feet,
+      inches: inches}
     })
     alert("You have successfully registered!")
-    //window.location = "login.html"
+    window.location = "login.html"
   })
   .catch(function(error) {
     var errorCode = error.code
