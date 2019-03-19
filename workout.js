876b6d8c81f06c7b135fb3fdb5a666b3f4a4dddc
Will Roberts <wcrober@gmail.com>
2019-03-19 16:05:04 -0500

    Added workouts and tutorial button as well as update look of button

---
+++ workout.js
@@ -0,0 +1,98 @@
+let workouts =  {
+    "level1": {
+        "legs":{
+               description: 'Leg Extension',
+                weight: 'light weight',
+                reps: 10,
+                sets: 2,
+                rest: '3 minutes'
+            },
+        "chest":{
+                description: 'Push Ups',
+                weight: 'Body weight',
+                reps: 15,
+                sets: 2,
+                rest: '3 minutes'
+            },
+        "arm":{
+                description: 'Curls',
+                weight: '15 lbs',
+                reps: 10,
+                sets: 2,
+                rest: '3 minutes'
+            },
+        "full Body":{
+                description: 'Jumping Jacks',
+                weight:'Body weight',
+                reps: 20,
+                sets: 3,
+                rest: '3 minutes'
+            }},
+    level2:{
+        "legs":{
+            description: 'lunges',
+            weight: '35 lbs',
+            reps: 12,
+            sets: 3,
+            rest: '2 minute'
+        },
+        "chest":{
+            description: 'Dips',
+            weight: 'Body weight',
+            reps: 15,
+            sets: 3,
+            rest: '2 minute'
+
+        },
+        "arm":{
+            description: 'Curls',
+            weight: '25 lbs',
+            reps: 10,
+            sets: 3,
+            rest: '2 minutes'
+        },
+        "full Body":{
+                description: 'Mountain Climber',
+                weight:'Body weight',
+                reps: 50,
+                sets: 3,
+                rest: '2 minutes'
+        }},
+    
+    level3:{
+        "legs":{
+            description: 'squates',
+            weight: '100 lbs',
+            reps: 15,
+            sets: 5,
+            rest: '1 minute'
+        },
+        "chest":{
+            description: 'Bench Press',
+            weight: '150 lbs',
+            reps: 15,
+            sets: 5,
+            rest: '1 minute'
+
+        },
+        "arm":{
+            description: 'Cable Curls',
+            weight: '40 lbs',
+            reps: 15,
+            sets: 5,
+            rest: '2 minutes'
+
+        },
+        "full Body":{
+            description: 'Burpies',
+            weight:'Body weight',
+            reps: 35,
+            sets: 5,
+            rest: '1 minutes'
+
+        }}
+
+}
+    
+    
+
